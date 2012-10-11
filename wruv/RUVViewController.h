d7d6e8c149c53266c8b1ceb06ad268c30bfe7c8f
Christopher Morse <dignanmorse@gmail.com>
2012-10-11 11:19:28 -0400

    Main view image, play button added
    - Added filler image for background UIImageView in player interface.
    - Added subview to contain player controls.
    - Add play button for testing streaming operation. To be replaced with control set.
    - Add WRUV URL
    - Add method for playing/stopping streaming using StreamModel.h
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- wruv/RUVViewController.h
+++ wruv/RUVViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <CFNetwork/CFNetwork.h>
 
 @interface RUVViewController : UIViewController
 
+
+@property (nonatomic, assign) IBOutlet UIImageView *stationArt;
+@property (nonatomic, assign) IBOutlet UIButton *playButton;
+@property (nonatomic, assign) IBOutlet UIView *playControl;
+@property (nonatomic, retain) NSString *m3uPath;
+@property (nonatomic, retain) NSURL *wruvLive;
+//@property (nonatomic, assign) IBOutlet UIToolbar *controlBar;
+//@property (nonatomic, assign) IBOutlet UIBarButtonItem *menuButton;
+
+- (IBAction) playToggle:(UIButton *)sender;
 @end
--- wruv/RUVViewController.m
+++ wruv/RUVViewController.m
@@ -8,19 +8,41 @@
 
 #import "RUVViewController.h"
 #import "IIViewDeckController.h"
+#import "StreamModel.h"
 
 @interface RUVViewController ()
-
+@property (nonatomic, retain) StreamModel *streamer;
 @end
 
 @implementation RUVViewController
+
+
+@synthesize stationArt = _stationArt;
+@synthesize streamer = _streamer;
+@synthesize m3uPath = _m3uPath;
+@synthesize playButton = playButton;
+@synthesize playControl = _playControl;
+
+//@synthesize menuButton = _menuButton;
+//@synthesize navigationBar = _navigationBar;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    self.streamer = [[StreamModel alloc] init];
+    //    self.m3uPath = @"http://139.140.232.18:8000/WBOR";
+    self.m3uPath = @"http://icecast.uvm.edu:8005/wruv_fm_256";
+    [self.playButton setBackgroundColor:[UIColor whiteColor]];
+    [[self.playButton titleLabel] setText:@"Play"];
+    
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"left" style:UIBarButtonItemStyleBordered target:self.viewDeckController action:@selector(toggleLeftView)];
+//    [self.menuButton setTitle:@"Back"];
+//    [self.menuButton setTarget:self.viewDeckController];
+//    [self.menuButton setAction:@selector(toggleLeftView)];
+    [self.stationArt setImage:[UIImage imageNamed:@"wruv.png"]];
+//    AVAudioPlayer
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,4 +51,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) playToggle:(UIButton *)sender
+{
+    if ([[[sender titleLabel] text] isEqualToString:@"Play"]){
+        [self.playButton setTitle:@"Stop" forState:UIControlStateNormal];
+        self.wruvLive = [[NSURL alloc] initWithString:self.m3uPath];
+        self.streamer = [[StreamModel alloc] initWithURL:self.wruvLive];
+        [self.streamer start];
+        [self.playButton setSelected:NO];
+    }
+    else if ([[[sender titleLabel] text] isEqualToString:@"Stop"]){
+        [self.playButton setTitle:@"Start" forState:UIControlStateNormal];
+        [self.streamer stop];
+        self.streamer = nil;
+    }
+}
 @end
