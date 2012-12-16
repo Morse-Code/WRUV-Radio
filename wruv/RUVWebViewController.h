b5bce76321bdb16f8a8f3804a3e9982c8c9cd391
Christopher Morse <dignanmorse@gmail.com>
2012-12-16 03:25:36 -0500

    RSS feeds
    - UVM RSS feeds displayed in center view.
    - Feed is selected from left view
    - Selecting article in RSS table displays item summary in detail view.
    - Selecting feed url in detail view presents full article in web view.
    - Web view has button for reformatting article as reading optimized text (Readability style).
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- wruv/RUVWebViewController.h
+++ wruv/RUVWebViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RUVWebViewController : UIViewController
+@interface RUVWebViewController : UIViewController < UIWebViewDelegate >
+
+
+@property NSString *urlString;
+
+@property (nonatomic) IBOutlet UIWebView *webView;
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *activityIndicator;
+
 
 @end
--- wruv/RUVWebViewController.m
+++ wruv/RUVWebViewController.m
@@ -8,11 +8,20 @@
 
 #import "RUVWebViewController.h"
 
+const NSString *textOnly = @"http://viewtext.org/api/text?url={url}&format={format}&callback={callback}";
+
 @interface RUVWebViewController ()
+
 
 @end
 
 @implementation RUVWebViewController
+
+
+@synthesize urlString = _urlString;
+@synthesize webView = _webView;
+@synthesize activityIndicator = _activityIndicator;
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,11 +32,75 @@
     return self;
 }
 
+
+- (void)startWebViewLoadFromString:(NSString *)string
+{
+
+    //NSString *urlAddress = @"http://www.google.com";
+    //Create a URL object.
+    NSURL *url = [NSURL URLWithString:string];
+
+    //URL Requst Object
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+
+    //Load the request in the UIWebView.
+    [self.webView loadRequest:requestObj];
+
+}
+
+
+- (void)webViewDidFinishLoad:(UIWebView *)thisWebView
+{
+
+    //stop the activity indicator when done loading
+    [self.activityIndicator stopAnimating];
+}
+
+
+- (void)showReadability
+{
+//    NSString *readJS = @"(function(){window.baseUrl='https://www.readability.com';window.readabilityToke‌​n='';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.se‌​tAttribute('charset','UTF-8');s.setAttribute('src',baseUrl+'/bookmarklet/read.js');document.documentElemen‌​t.appendChild(s);})()";
+    NSString *newString = [NSString stringWithFormat:@"http://viewtext.org/api/text?url=%@", self.urlString];
+//    [self startWebViewLoadFromString:readJS];
+//    NSString *newString = @"http://www.google.com";
+    [self.activityIndicator startAnimating];
+    [self performSelector:@selector(startWebViewLoadFromString:) withObject:newString afterDelay:0];
+
+}
+
+
 - (void)viewDidLoad
 {
+
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reader"
+                                                                              style:UIBarButtonItemStyleBordered
+                                                                             target:self
+                                                                             action:@selector(showReadability)];
+
+    //start an animator symbol for the webpage loading to follow
+    UIActivityIndicatorView *progressWheel = [[UIActivityIndicatorView alloc]
+                                                                       initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+
+    //makes activity indicator disappear when it is stopped
+    progressWheel.hidesWhenStopped = YES;
+
+    //used to locate position of activity indicator
+    progressWheel.center = CGPointMake(160, 160);
+
+    self.activityIndicator = progressWheel;
+    [self.view addSubview:self.activityIndicator];
+    [self.activityIndicator startAnimating];
+
+    [super viewDidLoad];
+
+    //call another method to do the webpage loading
+    [self performSelector:@selector(startWebViewLoadFromString:) withObject:self.urlString afterDelay:0];
+
+    // Do any additional setup after loading the view.
 }
+
 
 - (void)didReceiveMemoryWarning
 {
